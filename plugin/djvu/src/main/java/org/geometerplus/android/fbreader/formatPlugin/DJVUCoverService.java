98f2830baf7d5af2f84c3c08c67a8fed7e18451e
Nikolay Pultsin <geometer@fbreader.org>
2015-12-07 00:24:12 +0000

    DJVU plugin works with premium

--- plugin/djvu/src/main/java/org/geometerplus/android/fbreader/formatPlugin/DJVUCoverService.java
+++ plugin/djvu/src/main/java/org/geometerplus/android/fbreader/formatPlugin/DJVUCoverService.java
@@ -1,4 +1,11 @@
 package org.geometerplus.android.fbreader.formatPlugin;
 
+import org.geometerplus.fbreader.plugin.base.document.DJVUDocument;
+
 public class DJVUCoverService extends CoverService {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		DJVUDocument.init(this);
+	}
 }
--- plugin/djvu/src/main/java/org/geometerplus/android/fbreader/formatPlugin/DJVUCoverServicePremium.java
+++ plugin/djvu/src/main/java/org/geometerplus/android/fbreader/formatPlugin/DJVUCoverServicePremium.java
@@ -1,4 +1,11 @@
 package org.geometerplus.android.fbreader.formatPlugin;
 
+import org.geometerplus.fbreader.plugin.base.document.DJVUDocument;
+
 public class DJVUCoverServicePremium extends CoverService {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		DJVUDocument.init(this);
+	}
 }
