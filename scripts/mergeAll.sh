a82856af3cad20573f10ab1f6e74a631385368b2
Nikolay Pultsin <geometer@fbreader.org>
2017-04-04 07:26:35 +0100

    no non-existing branches refs

--- scripts/mergeAll.sh
+++ scripts/mergeAll.sh
@@ -3,15 +3,6 @@
 git checkout ice-cream-sandwich
 git merge master
 
-git checkout yota2
-git merge ice-cream-sandwich
-
-git checkout beta
-git merge yota2
-
-git checkout kindle
-git merge ice-cream-sandwich
-
 git checkout nook
 git merge master
 
