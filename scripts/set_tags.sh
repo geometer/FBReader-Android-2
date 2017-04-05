b29a27a54c855de476b8aadf604d2d686481c096
Nikolay Pultsin <geometer@fbreader.org>
2017-04-05 08:56:02 +0100

    no unexisting branches in set_tags script

--- scripts/set_tags.sh
+++ scripts/set_tags.sh
@@ -3,14 +3,7 @@
 git checkout master
 version=`cat fbreader/app/VERSION | sed 's/ //g'`
 git tag $version
-#git checkout ice-cream-sandwich
-git checkout yota2
-version=`cat fbreader/app/VERSION | sed 's/ //g'`
-git tag $version-ics
 git checkout nook
 version=`cat fbreader/app/VERSION | sed 's/ //g'`
 git tag $version-nst
-git checkout kindle
-version=`cat fbreader/app/VERSION | sed 's/ //g'`
-git tag $version-kindlefire
 git checkout master
