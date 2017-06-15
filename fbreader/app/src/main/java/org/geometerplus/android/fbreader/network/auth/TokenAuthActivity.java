10e02bf007f9deb69bfa939548fe6845d1fb314a
Nikolay Pultsin <geometer@fbreader.org>
2017-06-15 03:48:12 +0100

    year => 2017 in copyright notices

--- fbreader/app/src/main/java/org/geometerplus/android/fbreader/network/auth/TokenAuthActivity.java
+++ fbreader/app/src/main/java/org/geometerplus/android/fbreader/network/auth/TokenAuthActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2010-2016 FBReader.ORG Limited <contact@fbreader.org>
+ * Copyright (C) 2010-2017 FBReader.ORG Limited <contact@fbreader.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
